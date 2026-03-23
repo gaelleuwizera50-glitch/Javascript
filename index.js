f43639f5567d1f1f7158616d5790beb7bfadd6f7
gaelleuwizera50-glitch <gaelleuwizera50@gmail.com>
2026-03-23 16:00:01 +0300

    "Other data types"

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ console.log(country);
 // country= "Kenya"; This will throw an error because const cant be reassigned
 
 // Data types and they include; and typeof just shows the data type
+// Seven of them are called “primitive”, because their values contain only a single thing.
+
 // 1. String its values are in quotes
 let greeting= "Hello, How are you?";
 console.log(greeting);
@@ -34,3 +36,59 @@ console.log(x);
 console.log(y);
 console.log(typeof x);
 console.log(typeof y);
+
+
+// 3.BigInt  Used for Numeric values larger than the standard Number type can hold.
+// Dealing with very large numbers, such as IDs in large databases or precise scientific calculations.
+// A BigInt value is created by appending n to the end of an integer
+
+let z=1234567890123456789012345678901234567890n;
+const bigInt = 1234567890123456789012345678901234567890n;
+console.log(z);
+console.log(typeof z);
+
+// 4.Boolean (logical type)
+// The boolean type has only two values: true and false.
+// This type is commonly used to store yes/no values: true means “yes, correct”, and false means “no, incorrect”.
+
+console.log(5==6);
+console.log(5==5);
+
+
+// 5. Null value
+// Represents the intentional absence of any object value. It displays object
+// It’s just a special value which represents “nothing”, “empty” or “value unknown”.
+
+let q= null;
+console.log(q);
+// console.log(typeof q);
+
+// 6. Undefined value
+// The special value undefined also stands apart. It makes a type of its own, just like null.
+// The meaning of undefined is “value is not assigned”.
+// If a variable is declared, but not assigned, then its value is undefined:
+let height =100;
+height= undefined;
+// console.log(height);
+console.log(typeof height);
+
+
+// 7.Symbols Used for  unique identifiers.
+let value1 = Symbol("program");
+let value2 = Symbol("program");
+console.log(value1===value2);
+
+
+
+// 8. Objects is the only non-primitive data type: 
+// In contrast, objects are used to store keyed collections of various data and more complex entities
+// object  is used for more complex data structures.
+
+// student object
+const student = {
+    firstName: "Jack",
+    rollNo: 32, 
+};
+console.log(student);
+console.log(typeof student);
+
